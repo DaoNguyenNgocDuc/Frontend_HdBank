7bb11a328fd898aff3b10ce206a1fbdd42641f73
DaoNguyenNgocDuc <daoduc132@gmail.com>
2022-12-16 09:54:20 +0700

    new validate for transfer

--- src/transfer/Transfer.jsx
+++ src/transfer/Transfer.jsx
@@ -3,6 +3,7 @@ import "./transfer.css"
 import { useState } from 'react';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
+import Navbar from '../../components/navbar/Navbar';
 import { useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie';
 
@@ -67,49 +68,52 @@ function Transfer() {
     }
 
     return (
-        <div className='transferPage'>   
-            <div className='transferContainer'>
-                <div className='transferWraper'>
-                    <h2>Chức năng chuyển khoản</h2>
-                    <span className='textErrorMsg'>{modaltitle}</span>
-                    <form onSubmit={handleSubmit}>
-                        <div className='transferStyleInput'>
-                            <label>Số tài khoản người nhận</label>
-                            <input type="number" name="accountid" id="accountid" required
-                                onChange={(e) => setToAcc(e.target.value)} 
-                                onBlur = {(e) => {
-                                    if(e.target.value.length <6 || e.target.value.length>15) {
-                                        setErrorAcct("Số tài khoản trong khoảng từ 6 đến 15 ký tự!!!");
-                                    } else
-                                        setErrorAcct("");
-                                }} />
-                        </div>
-                        <div className='transferStyleInput'>
-                            <label>Số tiền chuyển khoản</label>
-                            <input type="number" name="amount" id="amount" required
-                                onChange={(e) => setAmount(e.target.value)} />
-                        </div>
+        <>
+            <div className='transferPage'>   
+                <div className='transferContainer'>
+                    <div className='transferWraper'>
+                        <h2>Chức năng chuyển khoản</h2>
+                        <span className='textErrorMsg'>{modaltitle}</span>
+                        <form onSubmit={handleSubmit}>
+                            <div className='transferStyleInput'>
+                                <label>Số tài khoản người nhận</label>
+                                <input type="number" name="accountid" id="accountid" required
+                                    onChange={(e) => setToAcc(e.target.value)} 
+                                    onBlur = {(e) => {
+                                        if(e.target.value.length <6 || e.target.value.length>15) {
+                                            setErrorAcct("Số tài khoản trong khoảng từ 6 đến 15 ký tự!!!");
+                                        } else
+                                            setErrorAcct("");
+                                    }} />
+                            </div>
+                            <div className='transferStyleInput'>
+                                <label>Số tiền chuyển khoản</label>
+                                <input type="number" name="amount" id="amount" required
+                                    onChange={(e) => setAmount(e.target.value)} />
+                            </div>
 
-                        <div className='transferStyleInput'>
-                            <label>Lời nhắn</label>
-                            <textarea type="text" name="description" id="description" required
-                                onChange={(e) => setDescription(e.target.value)}
-                                onBlur={e => {
-                                    if (e.target.value.length >= 151)
-                                        setErrorDescription("Lời nhắn không dài quá 150 ký tự!!!")
-                                    else
-                                        setErrorDescription("")
-                                }} />
-                            <span className='textErrorMsg'>{errorDescription}</span>
-                        </div>
+                            <div className='transferStyleInput'>
+                                <label>Lời nhắn</label>
+                                <textarea type="text" name="description" id="description" required
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    onBlur={e => {
+                                        if (e.target.value.length >= 151)
+                                            setErrorDescription("Lời nhắn không dài quá 150 ký tự!!!")
+                                        else
+                                            setErrorDescription("")
+                                    }} />
+                                <span className='textErrorMsg'>{errorDescription}</span>
+                            </div>
 
-                        <div className='transferSubmit'>
-                            <button type="submit" className='btnButton btnTransfer'>Xác nhận</button>
-                        </div>
-                    </form>
+                            <div className='transferSubmit'>
+                                <button type="submit" className='btnButton btnTransfer'>Xác nhận</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
+        
     )
 }
 
